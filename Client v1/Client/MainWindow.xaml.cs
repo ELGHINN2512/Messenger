fbe5e57817da51fe8c1b3adfbd8b1acd37314da4
ELGHINN2512 <65303418+ELGHINN2512@users.noreply.github.com>
2020-11-26 10:46:58 +0300

    Начало работы над клиентом мессенджера

---
+++ Client v1/Client/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Autorization(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Registration(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Owner = this;
+            registrationWindow.Width = this.ActualWidth;
+            registrationWindow.Height = this.ActualHeight;
+            Hide();
+            registrationWindow.Show();
+            registrationWindow.Closed += (s, args) => Close();
+        }
+    }
+}
